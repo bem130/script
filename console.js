6dd73753a024b14ddd5035aa31060ae5b2e2b837
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-04-24 01:45:48 +0900

    Update console.js

--- console.js
+++ console.js
@@ -5,7 +5,7 @@ if (document.getElementById("console") != null) {
           log(...args)
           const li = document.createElement('li');
           li.innerText = args[0];
-          document.getElementById('console').innerHTML += JSON.stringify(li);
+          document.getElementById('console').innerHTML += toString(li);
         }
         console.clear = function(){
           document.getElementById('console').innerHTML = "";
