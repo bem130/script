00c2c4c9d0c19651bac43bf2e8a77c884a4442dc
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-04-24 01:13:12 +0900

    Update console.js

--- console.js
+++ console.js
@@ -1,8 +1,8 @@
 if (document.getElementById("console") != null) {
     (function(){
       const log = console.log;
-      const error = console.log;
         console.log = function(...args){
+          log(...args)
           const li = document.createElement('li');
           li.innerText = args[0];
           document.getElementById('console').appendChild(li);
@@ -11,10 +11,7 @@ if (document.getElementById("console") != null) {
           document.getElementById('console').innerHTML = "";
         }
         console.error = function(...args){
-          const li = document.createElement('li');
-          li.innerText = args[0];
-          document.getElementById('console').innerHTML += "エラー" + JSON.stringify(li);
-        }
+          console.log(...args)
       })()
 }
 else {
