b66a942d93afa691bd04dde2ac2aecef84111aa3
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-04-23 23:51:32 +0900

    Update console.js

--- console.js
+++ console.js
@@ -1,24 +1,19 @@
 if (document.getElementById("console") != null) {
     (function(){
         const log = console.log;
-        const clear = console.clear;
-        const error = console.error;
         console.log = function(...args){
           log(...args);
           const li = document.createElement('li');
           li.innerText = args[0];
           document.getElementById('console').appendChild(li);
         }
-        console.clear = function(...args){
-          clear(...args);
-          const li = document.createElement('li');
-          li.innerText = args[0];
+        console.clear = function(){
           document.getElementById('console').innerHTML = "";
         }
         console.error = function(...args){
           args = "<span style='color:rgba(255,0,0,1)'>" + args + "</span>";
           args = args;
-          error(...args);
+          log(...args);
           const li = document.createElement('li');
           li.innerText = args[0];
           document.getElementById('console').appendChild(li);
