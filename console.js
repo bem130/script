c7c411880ac0b58145f8d4eb3794672fab5cb4a7
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-04-24 00:08:36 +0900

    Update console.js

--- console.js
+++ console.js
@@ -11,6 +11,7 @@ if (document.getElementById("console") != null) {
           document.getElementById('console').innerHTML = "";
         }
         console.error = function(...args){
+          console.log(typeof(args))
           args = "<span style='color:rgba(255,0,0,1)'>" + args + "</span>";
           args = args;
           console.log(typeof(args))
