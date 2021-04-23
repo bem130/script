ca7ee49b2768bb8865259dc17ca2fe4537b746a5
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-04-24 00:56:11 +0900

    Update console.js

--- console.js
+++ console.js
@@ -2,6 +2,7 @@ if (document.getElementById("console") != null) {
     (function(){
       const log = console.log
         console.log = function(...args){
+          log(...args)
           const li = document.createElement('li');
           li.innerText = args[0];
           document.getElementById('console').appendChild(li);
