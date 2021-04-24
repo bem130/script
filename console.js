44dc0d69440835ff9e04cf201119da8c31c4536e
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-04-24 14:48:17 +0900

    Update console.js

--- console.js
+++ console.js
@@ -4,23 +4,23 @@ if (document.getElementById("console") != null) {
       const error = console.error;
       const warn = console.warn;
         console.log = function(...args){
-          log(args[0],args[1])
+          log(...args)
           document.getElementById('console').innerHTML += "　  " + args + "<br>";
         }
         console.clear = function(){
           document.getElementById('console').innerHTML = "";
         }
         console.error = function(...args){
-          error(args)
+          error(...args)
           document.getElementById('console').innerHTML += "<span style='color:red'>　! " + args + "</span><br>";
         }
         console.warn = function(...args){
-          warn(args)
+          warn(...args)
           document.getElementById('console').innerHTML += "<span style='color:yellow'>　! " + args + "</span><br>";
         }
         console.message = function(...args){
-          document.getElementById('console').innerHTML += "　  " + args[0] + "<br>";
-          log(args[0],args[1])
+          document.getElementById('console').innerHTML += "　  " + args + "<br>";
+          console.log(...args)
         }
       })()
 }
@@ -30,4 +30,4 @@ else {
 }
 
 
-console.message("--console.js--",'background:#bbbcde')
+console.message("--console.js--")
